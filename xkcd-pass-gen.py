5472d5934889d61f31f7589f5bc283fc63ac31bf
Amelia Froemming <7919407+afroemming@users.noreply.github.com>
2018-07-16 15:38:40 -0500

    switch arg parsing from through sys to argparse

--- xkcd-pass-gen.py
+++ xkcd-pass-gen.py
@@ -1,19 +1,16 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 from random import choice, randint
-import sys
+import argparse
 
-try:
-    l = int(sys.argv[1])
-except:
-    l = 4
-try: 
-    n = int(sys.argv[2])
-except:
-    n = 10
+# Parse commandline arguments
+parser = argparse.ArgumentParser(description="Generate passwords in the style of XKCD 936")
+parser.add_argument("-w",  "--words", type=int, help="Number of words per password. Default 4.", default=4)
+parser.add_argument("-p", "--passwords", type=int, help="Number of passwords to generate. Default 10", default=10)
+args = parser.parse_args()
 
 with open('eff_large_wordlist.txt') as wordfile:
     wL = [line.split()[1] for line in wordfile]
 
-for i in range(n):
-    print(("{}-"*l+"{:03}").format(*[choice(wL) for x in range(l)], randint(0,1000)))
+for i in range(args.passwords):
+    print(("{}-"*args.words+"{:03}").format(*[choice(wL) for x in range(args.words)], randint(0,1000)))
     
