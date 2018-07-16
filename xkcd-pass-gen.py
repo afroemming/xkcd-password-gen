30ac76f1c96324749dc6b235f7abfaa6aa14d963
Amelia Froemming <7919407+afroemming@users.noreply.github.com>
2018-07-16 15:47:50 -0500

    added optional argument to append a number to each password

--- xkcd-pass-gen.py
+++ xkcd-pass-gen.py
@@ -6,11 +6,17 @@ import argparse
 parser = argparse.ArgumentParser(description="Generate passwords in the style of XKCD 936")
 parser.add_argument("-w",  "--words", type=int, help="Number of words per password. Default 4.", default=4)
 parser.add_argument("-p", "--passwords", type=int, help="Number of passwords to generate. Default 10", default=10)
+parser.add_argument("-a", "--append-numbers", 
+    help="If given, a three digit number is appended to the end of each password", action="store_true")
 args = parser.parse_args()
 
 with open('eff_large_wordlist.txt') as wordfile:
+    
     wL = [line.split()[1] for line in wordfile]
 
 for i in range(args.passwords):
-    print(("{}-"*args.words+"{:03}").format(*[choice(wL) for x in range(args.words)], randint(0,1000)))
-    
+    if args.append_numbers:
+        print(("{}-"*args.words+"{:03}").format(*[choice(wL) for x in range(args.words)], randint(0,1000)))
+    else:  
+         print(("{}-"*(args.words-1)+"{}").format(*[choice(wL) for x in range(args.words-1)], choice(wL)))
+        
